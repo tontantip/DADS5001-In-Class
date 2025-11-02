afbe90349b23f34561725127f7efd70f8df78348
Tontan Tipakun <129250727+tontantip@users.noreply.github.com>
2025-11-02 10:11:34 +0700

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Uber pickup in NYC")
+st.title("Uber pickup in NYC by New Tontan")
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
